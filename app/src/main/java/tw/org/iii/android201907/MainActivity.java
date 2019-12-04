f0444de87e60328f379087cc0e0a9ce96d6e4365
brad <brad@brad.tw>
2019-12-04 19:22:22 +0800

    1

---
+++ app/src/main/java/tw/org/iii/android201907/MainActivity.java
@@ -0,0 +1,20 @@
+package tw.org.iii.android201907;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
+    }
+    public void test2(View view) {
+    }
+}

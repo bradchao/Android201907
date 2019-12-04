5ca31ab0612e7a717222dfb3703ccf03d29616be
brad <brad@brad.tw>
2019-12-04 19:33:04 +0800

    2

--- app/src/main/java/tw/org/iii/android201907/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201907/MainActivity.java
@@ -2,18 +2,29 @@ package tw.org.iii.android201907;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = getSharedPreferences("mydata", MODE_PRIVATE);
+        editor = sp.edit();
     }
 
     public void test1(View view) {
+        editor.putBoolean("sound", false);
+        editor.putInt("stage", 4);
+        editor.putString("username", "Brad");
+        editor.commit();
+        Toast.makeText(this, "Save OK", Toast.LENGTH_SHORT).show();
     }
     public void test2(View view) {
     }
